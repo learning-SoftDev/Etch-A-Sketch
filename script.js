41b8f45dcfaf4aa3435fe243f72dbbdc06b8fd91
Mark Ramos <ramosmarkjoshua@gmail.com>
2022-10-29 10:26:50 +0800

    Slider & Color logic

--- script.js
+++ script.js
@@ -2,38 +2,28 @@
 let gridSlider = document.getElementById("gridSlider");
 let currentSize = gridSlider.value;
 let container = document.querySelector('.container');
+let gridSize = document.getElementById('gridSize');
+let colorPicker = document.getElementById('colorPicker');
 container.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
 container.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
-gridItemCreate();
+defGridItemCreate();
 
-//Events
-gridSlider.onchange = gridSliderOnChange
-
-
-let gridSize = document.getElementById('gridSize');
-
-gridSlider.addEventListener('mouseover', defMouseDownSlider);
-gridSlider.addEventListener('mousedown', defMouseDownSlider);
-
-
-function defMouseDownSlider(e){
-    if (e.type === 'mouseover' && !mouseDown){
-    } else {
-        gridSize.innerHTML = 'Grid Size: ' + currentSize
-    };
-}
-
-//Setting up fill logic
+//Mousedown logic
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-//
+//Slider logic
+gridSlider.onmousemove = defSliderMove;
+gridSlider.onclick = defSliderMove;
 
-
-
-
-
+//Eraser logic
+let buttonEraser = document.getElementById('buttonEraser')
+let buttonEraserDown = false
+buttonEraser.onclick = function(){
+    buttonEraserDown = !buttonEraserDown;
+    buttonEraserDown.style.backgroundColor = 'black'
+}
 
 
 
@@ -41,8 +31,31 @@ document.body.onmouseup = () => (mouseDown = false)
 
 //Functions
 
+//On change, delete current grid, set the new size, then recreate the grid
+function defGridSliderChange(){
+    defGridItemDelete();
+    currentSize = gridSlider.value;
+    defGridItemCreate();
+    container.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
+    return currentSize
+};
+  
+//On mousedown and mouseover, fill the cell
+function defMouseDown(e) {
+    if (e.type === 'mouseover' && !mouseDown){
+    } else {
+        if(buttonEraserDown){
+            e.target.style.backgroundColor = null; 
+        } else{
+            e.target.style.backgroundColor = colorPicker.value;
+        }
+        
+    };
+};
+
 //Create the grid items, append them in html, and put event listener
-function gridItemCreate(){
+function defGridItemCreate(){
     for(let i=1; i <= currentSize * currentSize; i++){        
         let div = document.createElement('div');
         div.setAttribute('class', 'gridItems');
@@ -52,28 +65,22 @@ function gridItemCreate(){
     };
 }; 
 
-//On change, delete current grid, set the new size, then recreate the grid
-function gridSliderOnChange(){
-    gridItemDelete();
-    currentSize = gridSlider.value;
-    gridItemCreate();
-    container.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`
-};
-  
 //Delete current grid
-function gridItemDelete(){
+function defGridItemDelete(){
     let div = document.querySelectorAll('.container > div')
     for(items of div){
         items.remove()
     };
 };
 
-//On mousedown and mouseover, fill the cell
-function defMouseDown(e) {
-    if (e.type === 'mouseover' && !mouseDown){
+//If the slider was changed, update grid size caption and grid itself
+function defSliderMove(e){
+    if (e.type === 'mousemove' && !mouseDown) {
     } else {
-        e.target.style.backgroundColor = 'red';
-    };
+        defGridSliderChange();
+        gridSize.innerHTML = 'Grid Size: ' + currentSize + ' x ' + currentSize;
+    }
 };
+
+
 
